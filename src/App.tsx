eab2935f4c8f5cdab8b6723de493720f2befb316
EKil <loreleyko@gmail.com>
2024-12-06 10:26:20 +0100

    added HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, createContext } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import UserPage from './pages/UserPage';
 import PetPage from './pages/PetPage';
 import ServicePage from './pages/ServicePage';
