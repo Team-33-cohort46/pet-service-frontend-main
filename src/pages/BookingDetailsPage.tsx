2866cac4ba91375d830ace0c088650a3a71cfcd0
Team-33-cohort46 <daniil.turovskiy24@gmail.com>
2024-12-12 10:50:52 +0100

    Update BookingDetailsPage.tsx

--- src/pages/BookingDetailsPage.tsx
+++ src/pages/BookingDetailsPage.tsx
@@ -86,7 +86,7 @@ const BookingDetailsPage: React.FC = () => {
   }
 
   return (
-    <div className="max-w-3xl mx-auto p-6 bg-gray-50 shadow-md rounded-lg">
+    <div className="max-w-3xl mx-auto p-6 bg-gray-50 shadow-md rounded-lg mt-6">
       <h1 className="text-3xl font-bold text-gray-800 mb-6 text-center">
         Booking Details
       </h1>
